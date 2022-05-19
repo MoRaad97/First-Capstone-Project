cfc77b29fb8cb6df531a2dc0689947b38aa0a226
Mohammed Raad <mohamedraad77@outlook.com>
2022-05-19 16:16:39 +0300

    Add linter configration and fix linter errors

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
-const navOpen = document.querySelector(".navbar-toggler");
-const navClose = document.querySelector(".close-btn-wrapper");
-const menuElement = document.querySelector(".menu");
-const linksAction = menuElement.querySelectorAll("a");
+const navOpen = document.querySelector('.navbar-toggler');
+const navClose = document.querySelector('.close-btn-wrapper');
+const menuElement = document.querySelector('.menu');
+const linksAction = menuElement.querySelectorAll('a');
 
-navOpen.addEventListener("click", () => {
-  menuElement.classList.remove("hidden");
-  document.querySelector("body").classList.add("overflow-hidden");
+navOpen.addEventListener('click', () => {
+  menuElement.classList.remove('hidden');
+  document.querySelector('body').classList.add('overflow-hidden');
 });
 
-navClose.addEventListener("click", () => {
-  menuElement.classList.add("hidden");
-  document.querySelector("body").classList.remove("overflow-hidden");
+navClose.addEventListener('click', () => {
+  menuElement.classList.add('hidden');
+  document.querySelector('body').classList.remove('overflow-hidden');
 });
 
 linksAction.forEach((node) => {
   node.onclick = () => {
-    menuElement.classList.add("hidden");
-    document.querySelector("body").classList.remove("overflow-hidden");
+    menuElement.classList.add('hidden');
+    document.querySelector('body').classList.remove('overflow-hidden');
   };
 });
---
+++ projectcontent.md
@@ -0,0 +1,19 @@
+html icon
+<i class="fa-brands fa-html5"></i>
+css icon
+<i class="fa-brands fa-css3-alt"></i>
+js icon
+<i class="fa-brands fa-js"></i>
+browser icon
+<i class="fa-light fa-browsers"></i>
+projects dumbbell icon
+<i class="fa-light fa-dumbbell"></i>
+
+nav bars icon
+<i class="fa-solid fa-bars"></i>
+
+h1>> Ultimate Arabic front-end course.
+
+p >> the ultimate front-end courses for arabic speakers, best Arabic "speakers" in the web development that give you the tools and knowledge to go further.
+
+p >> taking place in may in Egypt.
